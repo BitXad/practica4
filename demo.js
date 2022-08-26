123edec819084124d0fa04a70760a2910bb56f4c
Carlos Soto <r.carlos.soto@hotmail.com>
2022-08-25 22:27:14 -0400

    Subiendo archivos otras vez

---
+++ demo.js
@@ -0,0 +1,7 @@
+
+console.log("hola clase dia 6 - AGOSTO 25, 2022");
+console.log("Diplomado full stack");
+console.log("Otra prueba");
+console.log("Otra prueba para github");
+console.log("Otra prueba para github en el repositorio");
+
